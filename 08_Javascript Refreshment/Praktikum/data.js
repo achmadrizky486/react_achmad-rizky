47c92fe49aaca351842d8e19fb62480d951212cd
achmadrizky486 <achmadrizky486@gmail.com>
2022-03-10 16:26:20 +0700

    Add folder 8

---
+++ 08_Javascript Refreshment/Praktikum/data.js
@@ -0,0 +1,28 @@
+var a = 5;
+let b = "Kampus Merdeka";
+const nama = "Budi";
+let terdaftar = false;
+let lengkap_arr = [a, b, nama, terdaftar];
+
+function perkenalan() {
+    let asal = "indonesia";
+    return console.log(
+        "perkenalkan nama saya" +
+        nama +
+        "nomor urut" +
+        a +
+        "sekarang sedang mengikuti" +
+        b +
+        "berasal dari " +
+        asal
+    );
+}
+
+if (terdaftar === true) {
+    console.log(nama + " " + "terdaftar sebagai kegiatan kampus merdeka");
+}
+a = b;
+nama = b;
+console.log("asal diakses = " + asal);
+console.log("a adalah = " + a);
+console.log("b adalah = " + b);
---
+++ 08_Javascript Refreshment/Readme.md
@@ -0,0 +1,16 @@
+# Achmad Rizky 
+## Summary
+
+## ==== Javascript Refreshment ====
+
+- JavaScript adalah bahasa pemrograman tingkat tinggi dan dinamis. JavaScript populer di internet dan dapat bekerja di sebagian besar penjelajah web populer, dapat disisipkan dalam halaman web menggunakan tag SCRIPT.
+
+- Kelebihan JS diantaranya adalah :
+    1. Tidak butuh kompiler
+    2. Membuat web lebih menarik
+    3. Mudah dipelajari
+    4. Lebih cepat dan ringan daripada bahasa pemrograman lainnya
+    5. Dll
+
+- Bisa digunakan sebagai front end maupun back end
+

b45c7ba33535ba67ffdba902f018343f6e468295
achmadrizky486 <achmadrizky486@gmail.com>
2022-03-16 19:52:05 +0700

    Edit index2

---
+++ 09_Clean Code/Praktikum/index2.js
@@ -0,0 +1,20 @@
+class kendaraan {
+    var totalroda = 0;
+    var kecepatanperjam = 0;
+}
+class mobil extends kendaraan {
+    void berjalan() {
+        tambahkecepatan(10);
+    }
+    tambahkecepatan(var kecepatanbaru) {
+        kecepatanperjam = kecepatanperjam + kecepatanbaru;
+    }
+}
+void main() {
+    mobilcepat = new mobil();
+    mobilcepat.berjalan();
+    mobilcepat.berjalan();
+    mobilcepat.berjalan();
+    mobillamban = new mobil();
+    mobillamban.berjalan();
+}

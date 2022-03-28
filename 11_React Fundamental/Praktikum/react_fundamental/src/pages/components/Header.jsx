310af7ee88bc0e9b4bbacb1737ec57b4860421a6
achmadrizky486 <achmadrizky486@gmail.com>
2022-03-28 22:18:41 +0700

    Rename folder + pindah keluar

--- 11_React Fundamental/Praktikum/react_fundamental/src/pages/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import Styles from "./Todos.module.css"
-
-export default function Header ({text}){
-    return ( 
-        <div className={Styles.navbar}>
-        <h1 >{text}</h1>
-        </div>
-        )
-}
